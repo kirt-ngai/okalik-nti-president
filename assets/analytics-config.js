c9058530f759eaf74c39f92ba447781dfee38197
kirt-ngai <kirt@arcticuav.ca>
2026-05-28 15:55:15 -0400

    Delete assets directory

--- assets/analytics-config.js
+++
@@ -1,4 +0,0 @@
-window.OKALIK_ANALYTICS = {
-  site: "okalik-nti-president",
-  endpoint: "https://script.google.com/macros/s/AKfycbw1luYOewP_cjNVm7cTGr8rs-eeRkEOS9AlIXVB1dl1SGekI16czIDo3S9mUCUHmMSN/exec"
-};
